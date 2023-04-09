7ca024d173d319e0c052f7d516a9552c820bec19
nikkhildivate <129598002+nikkhildivate@users.noreply.github.com>
2023-04-10 00:29:27 +0530

    Add files via upload

---
+++ Assignment6_Q3/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  
+  <h1>
+  Marvellous Infosystems
+  </h1>
+  
+  <input type = "text">
+  `
+  ,
+  styles: 
+  
+  [`h1 {color:blue}`]
+})
+export class AppComponent {
+  title = 'Assignment06';
+}
